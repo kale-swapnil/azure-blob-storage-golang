b9901df2a9a27aacdf049626ef4579480701405d
kale-swapnil <swapnilnkale94@gmail.com>
2025-05-26 20:19:55 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kale-swapnil/azure-blob-storage-golang/blob"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Warning! .env file not found. Using default env vars.")
+	}
+	container := "golang"
+	blobName := "demo"
+	filePath := "./hello.txt"
+
+	fmt.Println("Calling Blob function")
+
+	err = blob.UploadBlob(container, blobName, filePath)
+	if err != nil {
+		fmt.Println("Upload failed:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Upload Successful")
+
+}
---
+++ upload.go
@@ -0,0 +1,42 @@
+package blob
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+)
+
+func UploadBlob(containerName, blobName, filePath string) error {
+	connStr := os.Getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+	if connStr == "" {
+		return fmt.Errorf("missing Azure connection string")
+	}
+
+	serviceClient, err := azblob.NewClientFromConnectionString(connStr, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create service client: %w", err)
+	}
+
+	containerClient := serviceClient.ServiceClient().NewContainerClient(containerName)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	blobClient := containerClient.NewBlockBlobClient(blobName)
+
+	_, err = blobClient.UploadStream(context.Background(), file, nil)
+	if err != nil {
+		return fmt.Errorf("failed to upload to blob: %w", err)
+	}
+
+	fmt.Println("Upload successful")
+
+	return nil
+
+}
